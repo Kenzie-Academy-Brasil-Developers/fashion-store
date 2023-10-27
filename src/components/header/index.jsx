9bafb85b2bb672bb41f19a1abd04f60c5a417693
Bernardo Stein <stein.bernardo@proton.me>
2023-10-27 20:16:37 -0300

    feat: created AppHeader component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+const AppHeader = () => {
+  return (
+    <header>
+      <div>
+        <img src="" alt="fashion logo" />
+        <MdOutlineShoppingCart size={20} />
+      </div>
+    </header>
+  );
+};
+
+export { AppHeader }
