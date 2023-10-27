fca993ee2d07da02e7e0b9ec43c6039f71a1949f
AndreyYuri <andreycaetano2102@gmail.com>
2023-10-27 19:34:52 -0300

    feat: creating request products

--- src/providers/requestProvider/index.jsx
+++ src/providers/requestProvider/index.jsx
@@ -1,11 +1,23 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export const requestsContext = createContext({})
 export const useRequestContext = useContext(requestsContext)
 
 export const RequestProvider = ({children}) => {
+    const [listProduct, setListProduct] = useState([])
+    
+    useEffect(async () => {
+        try {
+            const { data } = await api.get("/products")
+            setListProduct(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
     return(
-        <requestsContext.Provider value={{}}>
+        <requestsContext.Provider value={{listProduct}}>
             {children}
         </requestsContext.Provider>
     )
