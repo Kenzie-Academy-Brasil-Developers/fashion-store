e6385f256c346f8af7c4393418f5c9f2f8daef3d
Bernardo Stein <stein.bernardo@proton.me>
2023-10-28 15:26:42 -0300

    feat: create registerAdminForm

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -1,5 +1,6 @@
 import { StdInput } from "./inputs/index.jsx";
 import { useForm } from "react-hook-form";
+import { MdArrowBack } from "react-icons/md"
 
 export const LoginForm = () => {
 
@@ -34,3 +35,52 @@ export const LoginForm = () => {
     </form>
   );
 };
+
+export const RegisterForm = () => {
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submit = (payload) => {
+    console.log(payload);
+  };
+
+  return (
+    <div>
+      <button>
+        <MdArrowBack size={20} /> Voltar
+      </button>
+      <h2>Cadastrar-se</h2>
+      <p>Seja bem vindo, administrador!</p>
+      <form onSubmit={handleSubmit(submit)}>
+        <StdInput
+          type={"text"}
+          placeholder={"Nome"}
+          {...register("name")}
+          error={errors.name}
+        />
+        <StdInput
+          type={"email"}
+          placeholder={"Email"}
+          {...register("email")}
+          error={errors.email}
+        />
+        <StdInput
+          type={"password"}
+          placeholder={"Senha"}
+          {...register("password")}
+          error={errors.name}
+        />
+        <StdInput
+          type={"password"}
+          placeholder={"Confirmar senha"}
+          {...register("passValidate")}
+          error={errors.passValidate}
+        />
+      </form>
+    </div>
+  );
+}
