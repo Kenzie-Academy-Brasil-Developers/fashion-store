c378fc022bfadbb7ead93cd8bb123d4c670a55a5
Bernardo Stein <stein.bernardo@proton.me>
2023-10-28 12:35:18 -0300

    feat: update product list

--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -3,6 +3,7 @@ import { ProductCard } from "./productCard/index.jsx";
 export const ProductsList = () => {
   return (
     <section>
+      <h2>Produtos em Destaque</h2>
       <ul>
         <ProductCard />
       </ul>
