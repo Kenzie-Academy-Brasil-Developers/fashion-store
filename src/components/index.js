b53c07cfb0c2794d183935f8adf4b1be7e2bfece
Bernardo Stein <stein.bernardo@proton.me>
2023-10-28 15:45:41 -0300

    feat: create LoginPage

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,19 @@
 import { BannerSection } from "./bannerSection/index.jsx";
 import { DefaultTemplate } from "./defaultTemplate/index.jsx";
 import { ProductsList } from "./productList/index.jsx";
+import {
+  LoginForm,
+  RegisterAdminForm,
+  RegisterProductForm,
+  UpdateProductForm,
+} from "./forms/index.jsx";
 
-export { DefaultTemplate, BannerSection, ProductsList };
+export {
+  DefaultTemplate,
+  BannerSection,
+  ProductsList,
+  LoginForm,
+  RegisterAdminForm,
+  RegisterProductForm,
+  UpdateProductForm,
+};
