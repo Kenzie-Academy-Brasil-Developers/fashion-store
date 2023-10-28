1de8bac3d56e2e0b68b159d21815619995b241b3
AndreyYuri <andreycaetano2102@gmail.com>
2023-10-28 12:51:39 -0300

    feat: create logic login, auto login and logout

--- src/providers/requestProvider/index.jsx
+++ src/providers/requestProvider/index.jsx
@@ -1,8 +1,10 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 export const requestsContext = createContext({})
 export const useRequestContext = useContext(requestsContext)
+const navigate = useNavigate()
 
 export const RequestProvider = ({children}) => {
     const [listProduct, setListProduct] = useState([])
@@ -16,8 +18,41 @@ export const RequestProvider = ({children}) => {
         }
     }, [])
 
+    const getAutoLogin = async () => {
+        const user = localStorage.getItem("@FSToken")
+        if(user){
+            try {
+                const {data} = await api.get(`/users/${user.user.id}` ,{
+                    headers: {
+                        Authorization: `Barear ${user.accessToken}`
+                    }
+                })
+                navigate("/dashboard")
+            } catch (error) {
+                console.log(error)
+                localStorage.removeItem("@FSToken")
+            }
+        }
+    }
+
+    const login = async (formData) => {
+        try {
+            const {data} = await api.post("/login", formData)
+            localStorage.setItem("@FSToken", data)
+            navigate(("/dashboard"))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem("@FSToken")
+        navigate("/")
+    }
+
+
     return(
-        <requestsContext.Provider value={{listProduct}}>
+        <requestsContext.Provider value={{listProduct, getAutoLogin, login}}>
             {children}
         </requestsContext.Provider>
     )
