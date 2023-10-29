33904a7bb7a125ef54b94a31ad0aaa01d5daf885
Bernardo Stein <stein.bernardo@proton.me>
2023-10-29 10:39:25 -0300

    fix: create productPage

--- src/pages/productPage/index.jsx
+++ src/pages/productPage/index.jsx
@@ -1,1 +1,30 @@
-export const ProductPage = ''
+import { MdOutlineAddShoppingCart } from "react-icons/md";
+import { ProductsList } from "../../components";
+
+export const ProductPage = () => {
+  return (
+    <>
+      <h1>{`Home > ${"ProductName"}`}</h1>
+      <section>
+        <img src="" alt="Product image" />
+        <div>
+          <h2>ProductName</h2>
+          <p>ProductValue</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident,
+            saepe soluta quas laudantium nobis id qui dolorem labore libero vel
+            fugiat praesentium. Saepe omnis at modi reiciendis et autem
+            architecto?
+          </p>
+          <button><MdOutlineAddShoppingCart size={20}/> Adicionar ao carrinho</button>
+        </div>
+      </section>
+      <section>
+        <h2>Veja também</h2>
+        <ul>
+          {/* lista de produtos */}
+        </ul>
+      </section>
+    </>
+  );
+};
