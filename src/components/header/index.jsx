2ddf1cae303b677e39301f067be193c4ac1e116d
Bernardo Stein <stein.bernardo@proton.me>
2023-10-30 10:57:56 -0300

    pull develop

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,13 +2,22 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import logoImg from "../../assets/FAshionSTORE.png";
 
 export const AppHeader = () => {
+  const pathname = window.location.pathname
+  console.dir(pathname)
   return (
     <header>
       <div>
         <img src={logoImg} alt="fashion logo" />
-        <button>
-          <MdOutlineShoppingCart size={25} />
-        </button>
+        {pathname === "/" && (
+          <button>
+            <MdOutlineShoppingCart size={25} />
+          </button>
+        )}
+        {pathname === `/product/:id` && (
+          <button>
+            <MdOutlineShoppingCart size={25} />
+          </button>
+        )}
       </div>
     </header>
   );
