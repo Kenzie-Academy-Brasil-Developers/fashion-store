717d9b2d262b1596bdea4981a4c5de9ec7741bb9
João Lucas <joaolucasgusmao@hotmail.com>
2023-11-02 12:49:10 -0300

    feat: stylized header and adjusted logic

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,16 +2,22 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import logoImg from "../../assets/FAshionSTORE.png";
 import { useContext } from "react";
 import { productContext } from "../../providers/productsProvider";
+import styles from "./style.module.scss";
 
 export const AppHeader = () => {
   const { setCartIsOpen, cartCounter } = useContext(productContext);
   const pathname = window.location.pathname;
 
+  const divClassName =
+    pathname === "/" || pathname.includes("/product")
+      ? `${styles.div} ${styles.spaceBtw}`
+      : styles.div;
+
   return (
-    <header>
-      <div>
+    <header className={styles.header}>
+      <div className={divClassName}>
         <img src={logoImg} alt="fashion logo" />
-        {pathname === "/" || pathname.includes("product/") ? (
+        {pathname === "/" || pathname.includes("/product") ? (
           <button onClick={() => setCartIsOpen(true)}>
             <MdOutlineShoppingCart size={30} />
           </button>
