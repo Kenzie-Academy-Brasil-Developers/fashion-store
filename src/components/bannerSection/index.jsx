41060ef06447b01a3d6a022468edc5e846cab515
João Lucas <joaolucasgusmao@hotmail.com>
2023-11-03 12:02:42 -0300

    feat: homePage stylized

--- src/components/bannerSection/index.jsx
+++ src/components/bannerSection/index.jsx
@@ -1,8 +1,9 @@
 import bannerImg from "../../assets/banner.png";
+import styles from "./style.module.scss";
 
 const BannerSection = () => {
   return (
-    <section>
+    <section className={styles.section}>
       <img src={bannerImg} alt="banner image" />
       <div>
         <h1 className="title-1">KENZIE FASHION STORE</h1>
--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { ProductCard } from "./productCard/index.jsx";
 import { productContext } from "../../providers/productsProvider.jsx";
+import styles from "./style.module.scss";
 
 export const ProductsList = () => {
   const { listProduct } = useContext(productContext);
   return (
-    <section>
+    <section className={styles.section}>
       <h2 className="title-2">PRODUTOS EM DESTAQUE</h2>
-      <ul>
+      <ul className={styles.ul}>
         {listProduct?.map((product) => {
           return <ProductCard product={product} key={product.id} />;
         })}
