ac56d5238e58b156042c13035530f653c42785da
João Lucas <joaolucasgusmao@hotmail.com>
2023-11-03 21:12:21 -0300

    feat: main dashboard page stylized

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "./style.module.scss";
 
 export const Dashboard = () => {
   const [adminData, setAdminData] = useState({});
@@ -13,18 +14,22 @@ export const Dashboard = () => {
 
   return (
     <>
-      <nav>
-        <ul>
-          <Link to={"/dashboard"}>
-            <li className="navigation-title sm">INÍCIO</li>
-          </Link>
-          <Link to={"/dashboard/products"}>
-            <li className="navigation-title sm">PRODUTOS</li>
-          </Link>
-        </ul>
-      </nav>
-      <h1 className="title-2">PAINEL DO ADMINISTRADOR</h1>
-      <p className="paragraph">Seja bem vindo, {`${adminData}`}</p>
+      <div className={styles.flexBox}>
+        <nav>
+          <ul>
+            <Link to={"/dashboard"}>
+              <li className="navigation-title sm">INÍCIO</li>
+            </Link>
+            <Link to={"/dashboard/products"}>
+              <li className="navigation-title sm">PRODUTOS</li>
+            </Link>
+          </ul>
+        </nav>
+        <div>
+          <h1 className="title-2">PAINEL DO ADMINISTRADOR</h1>
+          <p className="paragraph">Seja bem vindo, {`${adminData}`}</p>
+        </div>
+      </div>
     </>
   );
 };
