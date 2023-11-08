49f3d25c793d2eb3a22b3d401679d13b01ebe890
Bernardo Stein <stein.bernardo@proton.me>
2023-11-08 12:57:08 -0300

    style: homepage animations

--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ProductCard } from "./productCard/index.jsx";
 import { productContext } from "../../providers/productsProvider.jsx";
 import styles from "./style.module.scss";
+import { motion } from "framer-motion";
 
 export const ProductsList = () => {
   const { listProduct } = useContext(productContext);
@@ -9,8 +10,17 @@ export const ProductsList = () => {
     <section className={styles.section}>
       <h2 className="title-2">PRODUTOS EM DESTAQUE</h2>
       <ul className={styles.ul}>
-        {listProduct?.map((product) => {
-          return <ProductCard product={product} key={product.id} />;
+        {listProduct?.map((product, idx) => {
+          return (
+            <motion.div
+              initial={{ opacity: 0, x: 150 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: idx * 0.2 }}
+              key={product.id}
+            >
+              <ProductCard product={product} />
+            </motion.div>
+          )
         })}
       </ul>
     </section>
